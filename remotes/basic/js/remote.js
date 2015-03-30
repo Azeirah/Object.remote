ab8b7abba3c947db012c6371059f634737da2902
Martijn Brekelmans <tijntje_7@msn.com>
2015-03-31 01:17:19 +0200

    small restructure, small update to remote and updated readme

---
+++ remotes/basic/js/remote.js
@@ -0,0 +1,32 @@
+var remote = remoteObject.remote("ws://localhost:8080");
+// hold references to cleanup functions to react elements, look in objectManipulator.js to see that component&objectManipulator return functions which are used to clean themselves up.
+var cleanups = {};
+
+remote.listenForCreation(function (object, namespace, id) {
+    if (namespace === "component") {
+        window.setTimeout(function () {
+            // append a span to the output area, so react can render a card inside it
+            var out = document.createElement("span");
+            document.querySelector("#output").appendChild(out);
+            cleanups[id] = componentManipulator(object, out, object.name);
+
+            object.onDelete(function () {
+                // remove the card whenever an object gets deleted
+                cleanups[id]();
+            });
+        }, 15);
+    }
+    if (namespace === "object") {
+        window.setTimeout(function () {
+            // append a span to the output area, so react can render a card inside it
+            var out = document.createElement("span");
+            document.querySelector("#output").appendChild(out);
+            cleanups[id] = objectManipulator(object, out, namespace);
+
+            object.onDelete(function () {
+                // remove the card whenever an object gets deleted
+                cleanups[id]();
+            });
+        }, 15);
+    }
+});
